22be84cd5479b8475768de0fd208d41c5ecbf1b0
👩Ladybug🐞 <psyjulga@yahoo.de>
2021-09-23 15:45:58 +0200

    added phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  return (
+    <div className="Phonetics">
+      <div className="written-phonetic">{props.data.text}</div>
+      <a href={props.data.audio} target="_blank">
+        🔊
+      </a>
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   if (props.data) {
     return (
       <div className="Results">
-        <h2 className="mb-3 mt-3"> {props.data.word} </h2>
+        <h3 className="mb-3 mt-3 d-inline"> {props.data.word} </h3>
+
+        {props.data.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics data={phonetic} />
+            </div>
+          );
+        })}
+        <br />
         {props.data.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
