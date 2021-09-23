0c132eccef34550b5e4d15ccb309ec3377bf201a
👩Ladybug🐞 <psyjulga@yahoo.de>
2021-09-23 15:47:59 +0200

    rel=noreferrer

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ export default function Phonetics(props) {
   return (
     <div className="Phonetics">
       <div className="written-phonetic">{props.data.text}</div>
-      <a href={props.data.audio} target="_blank">
+      <a href={props.data.audio} target="_blank" rel="noreferrer">
         🔊
       </a>
     </div>
